4761157adaf85ae5d403df0045abb2a829a3c73f
Hassan Ibrar <hassanibrar632@gmail.com>
2025-04-03 12:30:21 +0500

    added Setup file for the package
    Added the setup file for the package so that we can install the files and use that for our processing.

---
+++ RealESRGAN/__init__.py
@@ -0,0 +1,9 @@
+from .model import RealESRGAN
+
+"""
+__init__.py for the RealESRGAN module.
+
+This module provides the Real-ESRGAN model for image super-resolution.
+"""
+
+__all__ = ['RealESRGAN']
---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+import os
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='RealESRGAN',
+    version='1.0.0',
+    description='Real-ESRGAN: Practical Algorithms for General Image Restoration. This is an unofficial implementation of Real-ESRGAN',
+    author='M. Hassan Ibrar',
+    author_email='hassanibrar632@gmail.com',
+    url='https://github.com/Hassanibrar632/ESRGAN',
+    packages=find_packages(include=['RealESRGAN*']),
+    install_requires=requirements,
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
