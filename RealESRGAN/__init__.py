dfcdd5ea87688f884d64147dc41acd5a44aa3d7a
Hassan Ibrar <hassanibrar632@gmail.com>
2025-04-03 13:19:31 +0500

    Update __init__.py

--- RealESRGAN/__init__.py
+++ RealESRGAN/__init__.py
@@ -1,9 +1,1 @@
-from .model import RealESRGAN  # Import the class
-
-__all__ = ["RealESRGAN"]  # Set what gets imported when using `import RealESRGAN`
-
-# Define a direct alias for instantiation
-def __getattr__(name):
-    if name == "RealESRGAN":
-        return RealESRGAN
-    raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
+from .model import RealESRGAN
